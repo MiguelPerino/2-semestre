eb5b8270b6f9350ce92bb690e8535ddb973e4e29
MiguelP <77172963+MiguelPerino@users.noreply.github.com>
2025-08-13 14:28:54 -0300

    Update ex02.py

--- ex02.py
+++ ex02.py
@@ -1,37 +1,22 @@
 lista = []
-
-for i in range(5):
-    num = int(input(f'Informe o {i + 1}° número: '))
-    lista.append(num)
-
-lista_nova = []
-
-for i in range(len(lista)):
-    if lista[i] not in lista_nova:
-        lista_nova.append(lista[i])
-
-print(lista_nova)
-
-
-
-lista = []
-
-for i in range(5):
-    num = int(input(f'Informe o {i + 1}° número: '))
-    lista.append(num)
-
-lista_1 = []
-for i in range(len(lista)):
-    lista_1.append(lista[i])
+try:
+    for i in range(5):
+        num = int(input(f'Informe o {i + 1}° número: '))
+        lista.append(num)
+except ValueError:
+    print('Número inválido, apenas inteiros!')
 
 
 lista_nova = []
-igual = True
 for i in range(len(lista)):
-    for j in range(len(lista_1)):
-        if lista[i] == lista_1[j]:
+    igual = True
+
+    for j in range(len(lista_nova)):
+        if lista[i] == lista_nova[j]:
             igual = False
+            break
 
     if igual == True:
         lista_nova.append(lista[i])
+
 print(lista_nova)
