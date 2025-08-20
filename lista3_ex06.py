1dd87edc470d647a89bc6da65357e26616d35797
MiguelP <77172963+MiguelPerino@users.noreply.github.com>
2025-08-19 22:10:27 -0300

    Create lista3_ex06.py

---
+++ lista3_ex06.py
@@ -0,0 +1,7 @@
+# Usando novamente animais, percorra a tupla com enumerate() e exiba
+# na tela o índice e o valor de cada elemento. 
+
+animais = ('porco', 'ovelha', 'vaca', 'boi')
+
+for i, valor in enumerate(animais):
+    print(i, valor)
