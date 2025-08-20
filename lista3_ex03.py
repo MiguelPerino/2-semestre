30413e86a3eb7a2ad4212e3eb801eb440aa3a362
MiguelP <77172963+MiguelPerino@users.noreply.github.com>
2025-08-19 22:09:36 -0300

    Create lista3_ex03.py

---
+++ lista3_ex03.py
@@ -0,0 +1,9 @@
+#  Dada a tupla data = (15, 8, 2025), desempacote seus valores em três
+# variáveis (dia, mes, ano) e exiba no formato: 15/08/2025
+
+tupla = (15, 8 , 2025)
+dia = tupla[0]
+mes = tupla[1]
+ano = tupla[2]
+
+print(f'{dia}/{mes}/{ano}')
